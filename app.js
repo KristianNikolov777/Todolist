cdd416b50e9663300207126a03891adf2a8bb34d
Kristian Nikolov <kristiangeorgiev777@gmail.com>
2019-02-20 20:43:48 +0200

    Add procfile, gitignore and updated ports

--- app.js
+++ app.js
@@ -153,10 +153,13 @@ app.post("/delete", function(req, res) {
       }
     });
   }
-
-
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+
+}
+
+app.listen(port, function() {
+  console.log("Server has started succesfully");
 });
